8cc289a7744d715c8d1b1e20905bf735d7c09a85
jmarcelnm <jmarcelnm@outlook.com>
2025-01-30 18:45:21 +0100

    feat(event-list): adding the event listing page

--- src/app/features/event-list/event-list.component.ts
+++ src/app/features/event-list/event-list.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule, Router } from '@angular/router';
+import { EventService, Event } from '../../core/services/event/event.service';
 
 @Component({
   selector: 'app-event-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, MatCardModule, RouterModule],
   templateUrl: './event-list.component.html',
-  styleUrl: './event-list.component.css'
+  styleUrls: ['./event-list.component.css']
 })
-export class EventListComponent {
+export class EventListComponent implements OnInit {
+  events: Event[] = [];
 
+  constructor(private eventService: EventService, private router: Router) { }
+
+  ngOnInit() {
+    this.eventService.getEvents()
+      .subscribe((data) => {
+        this.events = data.sort((a, b) => a.endDate - b.endDate);
+      });
+  }
+
+  navigateToEvent(eventId: string) {
+    this.router.navigate(['/event', eventId]);
+  }
 }
