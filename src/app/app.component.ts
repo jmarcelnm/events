a9ff891b0b9906e0bac9006609ec54fbd29dba27
jmarcelnm <jmarcelnm@outlook.com>
2025-01-30 18:48:45 +0100

    refactor: improving the architecture, file structure, and code readability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { ShoppingCartComponent } from './shared/components/shopping-cart/shopping-cart.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule, RouterOutlet, ShoppingCartComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'events';
-}
+export class AppComponent { }
