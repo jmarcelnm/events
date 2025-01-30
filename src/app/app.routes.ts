0dbb2a069ce30c30bc3020e1f620ce5143594a1a
jmarcelnm <jmarcelnm@outlook.com>
2025-01-30 11:28:18 +0100

    feat(routes): adding routing for event details and list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { EventListComponent } from './features/event-list/event-list.component';
+import { EventDetailsComponent } from './features/event-details/event-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'events', pathMatch: 'full' },
+    { path: 'events', component: EventListComponent },
+    { path: 'event/:id', component: EventDetailsComponent },
+    { path: '**', redirectTo: 'events' }
+];
